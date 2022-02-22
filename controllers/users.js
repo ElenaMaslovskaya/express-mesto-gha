22ecf3013566b497f29927bdc93c7c85f1cfede6
Elena Maslovskaya <elena.maslovski@yandex.ru>
2022-02-22 12:23:07 +0100

    test5

--- controllers/users.js
+++ controllers/users.js
@@ -47,7 +47,7 @@ module.exports.updateUserInfo = (req, res) => {
   User.findByIdAndUpdate(
     req.user._id,
     { name, about },
-    { new: true },
+    { new: true, runValidators: true },
   )
     .then((user) => {
       if (!user) {
@@ -70,7 +70,7 @@ module.exports.updateUserAvatar = (req, res) => {
   User.findByIdAndUpdate(
     req.user._id,
     { avatar },
-    { new: true },
+    { new: true, runValidators: true },
   )
     .then((user) => {
       if (!user) {
